277b8f62e6b7aa99f5b2f233ee100563f7298c73
Anthony Picciano <picciano1@Anthonys-MacBook-Pro.local>
2015-02-05 11:05:02 -0700

    initial build

--- Comms/AppDelegate.m
+++ Comms/AppDelegate.m
@@ -7,39 +7,90 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
+#import <Parse/Parse.h>
+#import "Constants.h"
+#import "CocoaLumberjack.h"
 
 @interface AppDelegate ()
 
 @end
 
+static const DDLogLevel ddLogLevel = DDLogLevelError;
+
 @implementation AppDelegate
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [self initializeLumberjackWithOptions:launchOptions];
+    [self initializeParseWithOptions:launchOptions];
+    [self initializeUserInterfaceWithOptions:launchOptions];
+    
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+- (void)initializeLumberjackWithOptions:(NSDictionary *)launchOptions {
+    [DDLog addLogger:[DDASLLogger sharedInstance]];
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+- (void)initializeParseWithOptions:(NSDictionary *)launchOptions {
+    [Parse setApplicationId:PARSE_APPLICATION_ID
+                  clientKey:PARSE_CLIENT_KEY];
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+    
+    UIApplication *application = [UIApplication sharedApplication];
+    if (application.applicationState != UIApplicationStateBackground) {
+        // Track an app open here if we launch with a push, unless
+        // "content_available" was used to trigger a background push (introduced
+        // in iOS 7). In that case, we skip tracking here to avoid double
+        // counting the app-open.
+        BOOL preBackgroundPush = ![application respondsToSelector:@selector(backgroundRefreshStatus)];
+        BOOL oldPushHandlerOnly = ![self respondsToSelector:@selector(application:didReceiveRemoteNotification:fetchCompletionHandler:)];
+        BOOL noPushPayload = ![launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
+        if (preBackgroundPush || oldPushHandlerOnly || noPushPayload) {
+            [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+        }
+    }
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+- (void)initializeUserInterfaceWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    ViewController *viewController = [[ViewController alloc] initWithNibName:@"MainView" bundle:nil];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:viewController];
+    self.window.rootViewController = navigationController;
+    
+    [self.window makeKeyAndVisible];
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    // Store the deviceToken in the current Installation and save it to Parse.
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    [currentInstallation setObject:[PFUser currentUser] forKey:@"user"];
+    [currentInstallation setDeviceTokenFromData:deviceToken];
+    [currentInstallation saveInBackground];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    if (currentInstallation.badge != 0) {
+        currentInstallation.badge = 0;
+        [currentInstallation saveEventually];
+    }
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    [PFAnalytics trackAppOpenedWithRemoteNotificationPayload:userInfo];
+    NSString *message = userInfo[@"aps"][@"alert"];
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Remote Notification"
+                                                                   message:message
+                                                            preferredStyle:UIAlertControllerStyleActionSheet];
+    UIAlertAction *defaultAction = [UIAlertAction actionWithTitle:@"Okay"
+                                                            style:UIAlertActionStyleDefault
+                                                          handler:nil];
+    [alert addAction:defaultAction];
+    [self.window.rootViewController presentViewController:alert animated:YES completion:nil];
 }
 
 @end
---
+++ Comms/Constants.h
@@ -0,0 +1,39 @@
+//
+//  Constants.h
+//  Comms
+//
+//  Created by Anthony Picciano on 2/5/15.
+//  Copyright (c) 2015 Anthony Picciano. All rights reserved.
+//
+
+#ifndef Comms_Constants_h
+#define Comms_Constants_h
+
+
+#define PARSE_APPLICATION_ID        @"jtIeP2JlQH0dUvxA2rdt5apHBkiyOKlV6eZQFRDp"
+#define PARSE_CLIENT_KEY            @"xoDvMRxkCfqpcguTDiywONIgHjolunDL1gVvzZNJ"
+
+#define EMPTY_STRING                @""
+
+#define OBJECT_TYPE_CHANNEL         @"Channel"
+#define OBJECT_TYPE_GROUP           @"Group"
+#define OBJECT_TYPE_MESSAGE         @"Message"
+#define OBJECT_TYPE_SUBSCRIPTION    @"Subscription"
+
+#define OBJECT_KEY_USERNAME         @"username"
+#define OBJECT_KEY_PASSWORD         @"password"
+#define OBJECT_KEY_CREATED_AT       @"createdAt"
+#define OBJECT_KEY_UPDATED_AT       @"updatedAt"
+#define OBJECT_KEY_USER             @"user"
+#define OBJECT_KEY_GROUP            @"group"
+#define OBJECT_KEY_NAME             @"name"
+#define OBJECT_KEY_DISABLED         @"disabled"
+#define OBJECT_KEY_ORDER            @"order"
+#define OBJECT_KEY_CHANNEL          @"channel"
+#define OBJECT_KEY_RECIPIENT        @"recipient"
+#define OBJECT_KEY_TEXT             @"text"
+
+#define CURRENT_USER_CHANGE_NOTIFICATION    @"currentUserChange"
+
+
+#endif
