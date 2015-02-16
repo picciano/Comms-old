3b8df8106d5e585abd8ac6b225a7fe50c07a9d18
Anthony Picciano <picciano1@Anthonys-MacBook-Pro.local>
2015-02-16 14:15:33 -0700

    add lock icon to hidden channels

--- Comms/ChannelTableViewCell.m
+++ Comms/ChannelTableViewCell.m
@@ -8,12 +8,20 @@
 
 #import "ChannelTableViewCell.h"
 #import "Constants.h"
+#import "StyleKit.h"
 
 @implementation ChannelTableViewCell
 
 - (void)setChannel:(PFObject *)channel {
     _channel = channel;
     self.textLabel.text = [self.channel objectForKey:OBJECT_KEY_NAME];
+    
+    BOOL hidden = [[self.channel objectForKey:OBJECT_KEY_HIDDEN] boolValue];
+    if (hidden) {
+        self.imageView.image = [StyleKit imageOfChannelLock];
+    } else {
+        self.imageView.image = nil;
+    }
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
